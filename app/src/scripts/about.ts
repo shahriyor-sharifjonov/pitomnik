51089d95939710011280075ea54a59d9bbdbbf96
Shahriyor <shahriyordeveloping@gmail.com>
2021-12-26 22:44:39 +0500

    initial

--- app/src/scripts/about.ts
+++ app/src/scripts/about.ts
@@ -31,3 +31,36 @@ var swiper = new Swiper(".gallery__swiper", {
         },
       },
 });
+var swiper = new Swiper(".check__swiper", {
+    cssMode: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    grabCursor: true,
+    navigation: {
+      nextEl: ".gallery-button-next",
+      prevEl: ".gallery-button-prev",
+    },
+    pagination: {
+      el: ".gallery-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        992: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+      },
+});
