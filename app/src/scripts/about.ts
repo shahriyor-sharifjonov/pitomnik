f4b61a56d9e02e40351e24c62d77fd540bddd960
Shahriyor <shahriyordeveloping@gmail.com>
2021-12-27 12:00:34 +0500

    initial

--- app/src/scripts/about.ts
+++ app/src/scripts/about.ts
@@ -3,6 +3,7 @@ var swiper = new Swiper(".gallery__swiper", {
     slidesPerView: 1,
     spaceBetween: 20,
     grabCursor: true,
+    centeredSlides: true,
     navigation: {
       nextEl: ".gallery-button-next",
       prevEl: ".gallery-button-prev",
@@ -16,18 +17,54 @@ var swiper = new Swiper(".gallery__swiper", {
         576: {
             slidesPerView: 1,
             spaceBetween: 20,
+            centeredSlides: true,
         },
         640: {
           slidesPerView: 2,
           spaceBetween: 20,
+          centeredSlides: false,
         },
         768: {
           slidesPerView: 3,
           spaceBetween: 30,
+          centeredSlides: false,
         },
         992: {
           slidesPerView: 4,
           spaceBetween: 30,
+          centeredSlides: false,
+        },
+      },
+});
+var swiper = new Swiper(".help__swiper", {
+    cssMode: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    grabCursor: true,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".gallery-button-next",
+      prevEl: ".gallery-button-prev",
+    },
+    pagination: {
+      el: ".gallery-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            centeredSlides: true,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+          centeredSlides: false,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          centeredSlides: false,
         },
       },
 });
@@ -36,6 +73,7 @@ var swiper = new Swiper(".check__swiper", {
     slidesPerView: 1,
     spaceBetween: 20,
     grabCursor: true,
+
     navigation: {
       nextEl: ".gallery-button-next",
       prevEl: ".gallery-button-prev",
